cfa046e9a8860d1b287716b2d867d4c77852b6d7
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2022-12-04 08:53:08 +0700

    feat: php logic for authen

--- login.php
+++ login.php
@@ -1,16 +1,16 @@
 <?php
-    $db = new mysqli("localhost","root","","user_db");
+    $db = new mysqli("localhost","root","","electronic-ecommerce");
     if($db->connect_error){
         die("Connection failed: " . $db->connect_error);
     }
-    if(trim($_POST["email"]) != "" && trim($_POST["pwd"]) != ""){
+    if(trim($_POST["email"]) != "" && trim($_POST["password"]) != ""){
         $email = $_POST["email"];
-        $pwd = $_POST["pwd"];
-        $query = "SELECT * FROM user_data WHERE email='$email'";
+        $password = $_POST["password"];
+        $query = "SELECT * FROM user_account WHERE email='$email'";
         $res = $db->query($query);
         if($res->num_rows > 0){
             while($row = $res->fetch_assoc()){
-                if($row["password"] == $pwd){
+                if($row["password"] == $password){
                     echo "<h2>Đăng nhập thành công!<h2>";
                     break;
                 }
--- register.php
+++ register.php
@@ -1,23 +1,24 @@
 <?php
-    $db = new mysqli("localhost","root","","user_db");
+    $db = new mysqli("localhost","root","","electronic-ecommerce");
     if($db->connect_error){
         die("Connection failed: " . $db->connect_error);
     }
-    if(trim($_POST["username"]) != "" && trim($_POST["email"]) != "" && trim($_POST["pwd"]) != "" && trim($_POST["cf-pwd"]) != ""){
+    if(trim($_POST["username"]) != "" && trim($_POST["email"]) != "" && trim($_POST["password"]) != "" && trim($_POST["cf-password"]) != ""){
         $username = $_POST["username"];
         $email = $_POST["email"];
-        $pwd = $_POST["pwd"];
-        $cf_pwd = $_POST["cf-pwd"];
-        $query = "SELECT * FROM user_data WHERE email='$email'";
+        $password = $_POST["password"];
+        $cf_password = $_POST["cf-password"];
+        
+        $query = "SELECT * FROM user_account WHERE email='$email'";
         $res = $db->query($query);
         if($res->num_rows > 0){
             echo "<h2>Email đã được sử dụng</h2>";
         }
-        else if($pwd != $cf_pwd){
+        else if($password != $cf_password){
             echo "<h2>Mật khẩu xác nhận không đúng</h2>";
         }
         else{
-            $query = "INSERT INTO user_data (username,email,password) VALUES ('$username','$email','$pwd')";
+            $query = "INSERT INTO user_account (username,email,password) VALUES ('$username','$email','$password')";
             $db->query($query);
             echo "<h2>Đăng kí thành công!</h2>";
         }
