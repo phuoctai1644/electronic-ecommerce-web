e7c0495447dfa520e14f5cd69c12b8a03a2ef260
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2022-12-11 18:07:44 +0700

    fix: add product to cart and direct to product detail when click

--- main.js
+++ main.js
@@ -7,3 +7,12 @@ addEventListener('load', () => {
   const cartNumber = document.querySelector('.header__action-cart-number');
   cartNumber.innerHTML = localStorage.getItem('cart-quantity') ?? 0;
 })
+
+function addToCart() {
+  const cartNumber = document.querySelector('.header__action-cart-number');
+  let quantity = localStorage.getItem('cart-quantity') ?? 0;
+
+  cartNumber.innerHTML = ++quantity;
+
+  localStorage.setItem('cart-quantity', quantity++);
+}
